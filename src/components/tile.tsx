43c3d0c778e6588284396913aaae501b3413ee90
Santiago C <3glbbv5pg@mozmail.com>
2023-03-13 19:31:31 -0300

    abstraction for tiles

--- src/components/tile.tsx
+++ src/components/tile.tsx
@@ -5,25 +5,48 @@ import styles from 'styles/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-function Tile() {
+function Tile(...{size = 'normal', name = 'Jane Doe', image = 'https://static.platzi.com/media/learningpath/badges/08261d05-1b55-4c23-b9b4-8427d4247835.jpg'}) {
+  const dimensions =[];
+  switch(size){
+    case 'small':
+      dimensions.push(120);
+        dimensions.push(120);
+       
+      break;
+      case 'normal':
+        dimensions.push(120);
+        dimensions.push(120);
+       
+        break;
+        case 'big':
+          dimensions.push(120);
+        dimensions.push(120);
+       
+          break;
+          default:
+            dimensions.push(120);
+        dimensions.push(120);
+      
+            break;
+  }
   return (
     <>
     
     
     <div className="container mt-3" style={{maxWidth:250, padding:0, margin:0}} >
  
-  <div className="card container-fluid  " style={{width:120,  height:120, "border":"2px solid black"}} >
-    <center className="navbar-brand"><Image className=" rounded-pill" src="https://static.platzi.com/media/learningpath/badges/08261d05-1b55-4c23-b9b4-8427d4247835.jpg" alt="Card image" width={75} height ={75} style={{marginTop: 5, padding:0 }} /></center>
+  <div className="card container-fluid  " style={{width:120,  height:dimensions[1], "border":"2px solid black"}} >
+    <center className="navbar-brand"><Image className=" rounded-pill" src={image} alt="Card image" width={75} height ={75} style={{marginTop: 5, padding:0 }} /></center>
     
     <div className="card-img-overlay d-flex align-items-end" style={{marginTop:10, padding:0, marginLeft:8  }}>
-      <p className="card-title" >John Doe</p>
+      <p className="card-title" >{name} </p>
       
     </div>
   </div>
 </div>
 
     </>
-  )
+  );
 }
 
 export {Tile};
